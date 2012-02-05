753f0df89e76366a5ae6928c9a27a1bed2de3cea
Jason Kraus <jasonk@cukerinteractive.com>
2012-02-05 12:53:35 -0800

    added adminAwareSchemaAdmin

--- schemamaker/admin.py
+++ schemamaker/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 
-from dockit.admin.documentadmin import DocumentAdmin
+from dockit.admin.documentadmin import DocumentAdmin, SchemaAdmin
 
 from schemamaker.models import DocumentDesign
 
 #TODO consider a better name
+class AdminAwareSchemaAdmin(SchemaAdmin):
+    def get_form_class(self, request, obj=None):
+        if hasattr(self.schema, 'get_admin_form_class'):
+            form_class = self.schema.get_admin_form_class()
+            if form_class:
+                return form_class
+        return super(AdminAwareDocumentAdmin, self).get_form_class(request, obj)
+
 class AdminAwareDocumentAdmin(DocumentAdmin):
+    default_schema_admin = AdminAwareSchemaAdmin
+    
     def get_admin_class_for_schema(self, schema):
         for cls, admin_class in self.schema_inlines:
             if schema == cls:
