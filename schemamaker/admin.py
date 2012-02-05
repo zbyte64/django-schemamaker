b5766f9a9cbb8635da17a77974c79f4616a4fe43
Jason Kraus <jasonk@cukerinteractive.com>
2012-02-04 19:21:56 -0800

    admin functionality shift

--- schemamaker/admin.py
+++ schemamaker/admin.py
@@ -4,8 +4,19 @@ from dockit.admin.documentadmin import DocumentAdmin
 
 from schemamaker.models import DocumentDesign
 
-class DocumentDesignAdmin(DocumentAdmin):
+#TODO consider a better name
+class AdminAwareDocumentAdmin(DocumentAdmin):
+    def get_admin_class_for_schema(self, schema):
+        for cls, admin_class in self.schema_inlines:
+            if schema == cls:
+                return admin_class
+        if hasattr(schema, 'get_admin_class'):
+            return schema.get_admin_class()
+        return self.default_schema_admin
+
+class DocumentDesignAdmin(AdminAwareDocumentAdmin):
     pass
+    
 
 admin.site.register([DocumentDesign], DocumentDesignAdmin)
 
